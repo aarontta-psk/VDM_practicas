1a7872787db9ba2dbc2afa39101fab4950b0951f
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2022-09-18 16:51:18 +0200

    acabao sin opcionales porque pereza

--- P0/hello_world_mobile/src/main/java/com/example/hello_world_mobile/DisplayMessageActivity.java
+++ P0/hello_world_mobile/src/main/java/com/example/hello_world_mobile/DisplayMessageActivity.java
@@ -6,12 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivityDos extends AppCompatActivity {
+public class DisplayMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_dos);
+        setContentView(R.layout.activity_display_message);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
--- P0/hello_world_mobile/src/main/java/com/example/hello_world_mobile/MainActivity.java
+++ P0/hello_world_mobile/src/main/java/com/example/hello_world_mobile/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         // Do something in response to button
-        Intent intent = new Intent(this, MainActivityDos.class);
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
