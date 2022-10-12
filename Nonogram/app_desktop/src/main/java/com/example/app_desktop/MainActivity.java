524053cb33c29e84e9c17397635b527f8ee95adc
Alberto <albemu07@ucm.es>
2022-10-12 10:06:40 +0200

    sonidos primer cosa

--- Nonogram/app_desktop/src/main/java/com/example/app_desktop/MainActivity.java
+++ Nonogram/app_desktop/src/main/java/com/example/app_desktop/MainActivity.java
@@ -1,5 +1,15 @@
 package com.example.app_desktop;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.JFrame;
 
 public class MainActivity {
@@ -39,5 +49,43 @@ public class MainActivity {
         render.resume();
     }
 
+    public class MyMouseListener implements MouseListener {
 
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            try {
+                File audioFile = new File("sounds/doFlauta.wav");
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
+            }
+            catch (UnsupportedAudioFileException | IOException unsupportedAudioFileException) {
+                unsupportedAudioFileException.printStackTrace();
+            }
+
+            try {
+                Clip clip = AudioSystem.getClip();
+            } catch (LineUnavailableException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
+    }
 }
