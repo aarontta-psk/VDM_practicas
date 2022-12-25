592f2b9bea46a6937afb9a69da8ed4310fd918e2
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2022-12-25 21:59:39 +0100

    Ahora los tablero aleatorios tienes más filas que columnas

--- Nonogram/nonogram/src/main/java/com/example/nonogram/Board.java
+++ Nonogram/nonogram/src/main/java/com/example/nonogram/Board.java
@@ -25,214 +25,227 @@ public class Board {
     private int height, width;
     private int posX = 0, posY = 0;
 
-    private String font;
     private String fontWrongText;
 
     private int maxNumbers;
     private List<Cell> checkedCells;
     private double lastTimeChecked;
 
-    public void init(int h, int w) {
-        height = h;
-        width = w;
-        board = new Cell[width][height];
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                board[i][j] = new Cell();
-            }
-        }
+    public void init(int w, int h) {
+        this.width = w;
+        this.height = h;
+        this.board = new Cell[this.width][this.height];
+        for (int i = 0; i < this.width; i++)
+            for (int j = 0; j < this.height; j++)
+                this.board[i][j] = new Cell();
 
-        cols = new ArrayList[width];
-        rows = new ArrayList[height];
         Random random = new Random();
+        this.cols = new ArrayList[width];
+        this.rows = new ArrayList[height];
+        this.checkedCells = new ArrayList<>();
 
-        checkedCells = new ArrayList<Cell>();
-
-        maxNumbers = 1;
-        for (int i = 0; i < height; i++) {
-            rows[i] = new ArrayList<Integer>();
-            rows[i].add(-1);
-            if (maxNumbers < rows[i].size())
-                maxNumbers = rows[i].size();
+        this.maxNumbers = 1;
+        for (int i = 0; i < this.height; i++) {
+            this.rows[i] = new ArrayList<>();
+            this.rows[i].add(-1);
+            if (this.maxNumbers < rows[i].size())
+                this.maxNumbers = rows[i].size();
         }
 
-        for (int i = 0; i < width; i++) {        //Creación aleatoria del tablero
-            cols[i] = new ArrayList<Integer>();
-            cols[i].add(-1);
-            for (int j = 0; j < height; j++) {
+        // creación aleatoria del tablero
+        for (int i = 0; i < this.width; i++) {
+            this.cols[i] = new ArrayList<>();
+            this.cols[i].add(-1);
+            for (int j = 0; j < this.height; j++) {
                 int rand = random.nextInt(10);
-                board[i][j].init(rand < 4);
+                this.board[i][j].init(rand < 4);
                 if (rand < 4) {
-                    cellsLeft++;
+                    this.cellsLeft++;
 
-                    if (cols[i].get(cols[i].size() - 1) == -1) {      //Rellenado vector columnas
-                        cols[i].remove(cols[i].size() - 1);
-                        cols[i].add(1);
-                        if (maxNumbers < cols[i].size())
-                            maxNumbers = cols[i].size();
-                    } else {
-                        cols[i].set(cols[i].size() - 1, cols[i].get(cols[i].size() - 1) + 1);
-                    }
+                    // rellenado vector columnas
+                    if (this.cols[i].get(this.cols[i].size() - 1) == -1) {
+                        this.cols[i].remove(this.cols[i].size() - 1);
+                        this.cols[i].add(1);
+                        if (this.maxNumbers < cols[i].size())
+                            this.maxNumbers = cols[i].size();
+                    } else
+                        this.cols[i].set(this.cols[i].size() - 1, this.cols[i].get(this.cols[i].size() - 1) + 1);
 
-                    if (rows[j].get(rows[j].size() - 1) == -1) {      //Rellenado vector filas
-                        rows[j].remove(rows[j].size() - 1);
-                        rows[j].add(1);
-                        if (maxNumbers < rows[j].size())
-                            maxNumbers = rows[j].size();
-                    } else {
-                        rows[j].set(rows[j].size() - 1, rows[j].get(rows[j].size() - 1) + 1);
-                    }
+                    // rellenado vector filas
+                    if (this.rows[j].get(this.rows[j].size() - 1) == -1) {
+                        this.rows[j].remove(this.rows[j].size() - 1);
+                        this.rows[j].add(1);
+                        if (this.maxNumbers < this.rows[j].size())
+                            this.maxNumbers = this.rows[j].size();
+                    } else
+                        this.rows[j].set(this.rows[j].size() - 1, this.rows[j].get(this.rows[j].size() - 1) + 1);
                 } else {   //Añadimos -1 al vector de filas y columnas si no es solucion y la casilla anterior si
-                    if (cols[i].get(cols[i].size() - 1) != -1)
-                        cols[i].add(-1);
-                    if (rows[j].get(rows[j].size() - 1) != -1)
-                        rows[j].add(-1);
+                    if (this.cols[i].get(this.cols[i].size() - 1) != -1)
+                        this.cols[i].add(-1);
+                    if (this.rows[j].get(this.rows[j].size() - 1) != -1)
+                        this.rows[j].add(-1);
                 }
             }
-            if (cols[i].get(cols[i].size() - 1) != -1)
-                cols[i].add(-1);
+            if (this.cols[i].get(this.cols[i].size() - 1) != -1)
+                this.cols[i].add(-1);
         }
 
-        for (int i = 0; i < height; i++) {
-            if (rows[i].get(rows[i].size() - 1) != -1)
-                rows[i].add(-1);
+        for (int i = 0; i < this.height; i++) {
+            if (this.rows[i].get(this.rows[i].size() - 1) != -1)
+                this.rows[i].add(-1);
         }
+
         int maxDimension = Math.max(w, h);
-
         int winW = (GameManager.getInstance().getWidth()) / (maxDimension + maxDimension / 8);
-        int winH = ((int)(GameManager.getInstance().getHeight() / 1.85) - maxNumbers * fontSize) / (maxDimension + maxDimension / 8);
+        int winH = ((int) (GameManager.getInstance().getHeight() / 1.85) - maxNumbers * fontSize) / (maxDimension + maxDimension / 8);
 
-        board_cell_size = Math.min(winH, winW);
-        separation_margin = Math.max(board_cell_size / 25, 1);
-        board_cell_size -= separation_margin;
-        fontSize = board_cell_size / 3;
+        this.board_cell_size = Math.min(winH, winW);
+        this.separation_margin = Math.max(this.board_cell_size / 25, 1);
+        this.board_cell_size -= this.separation_margin;
+        this.fontSize = this.board_cell_size / 3;
 
-        posX = (GameManager.getInstance().getWidth() - (board_cell_size + separation_margin) * width - maxNumbers * fontSize) / 2;
-        posY = ((int) (GameManager.getInstance().getHeight() / 0.75f) - (board_cell_size + separation_margin) * height - maxNumbers * fontSize) / 2;
+        this.posX = (GameManager.getInstance().getWidth() - (this.board_cell_size + this.separation_margin) * this.width
+                - this.maxNumbers * this.fontSize) / 2;
+        this.posY = ((int) (GameManager.getInstance().getHeight() / 0.75f) - (this.board_cell_size + this.separation_margin) * this.height
+                - maxNumbers * fontSize) / 2;
 
-//        font = eng.getRender().loadFont("fonts/SimplySquare.ttf", FontType.DEFAULT, fontSize);
-        fontWrongText = Resources.FONT_SIMPLY_SQUARE_BIG;
-        lastTimeChecked = -1;
+        // error message setup
+        this.fontWrongText = Resources.FONT_SIMPLY_SQUARE_BIG;
+        this.lastTimeChecked = -1;
     }
 
     public void render(IRender renderMng) {
-        renderMng.setColor(0xFF000000); //Cuadrados alrededor
-        renderMng.drawRectangle(maxNumbers * fontSize + posX, posY + maxNumbers * fontSize, width * (board_cell_size + separation_margin) + 1,
-                height * (board_cell_size + separation_margin) + 1, false);
-        renderMng.drawRectangle(posX + maxNumbers * fontSize, maxNumbers * fontSize + posY, width * (board_cell_size + separation_margin) + 1,
-                height * (board_cell_size + separation_margin) + 1, false);
+        renderMng.setColor(0xFF000000); // board limits
+        renderMng.drawRectangle(this.maxNumbers * this.fontSize + this.posX, this.posY + this.maxNumbers * this.fontSize,
+                this.width * (this.board_cell_size + this.separation_margin) + 1,
+                this.height * (this.board_cell_size + this.separation_margin) + 1, false);
+        renderMng.drawRectangle(this.posX + this.maxNumbers * this.fontSize, this.maxNumbers * this.fontSize + this.posY,
+                this.width * (this.board_cell_size + this.separation_margin) + 1,
+                this.height * (this.board_cell_size + this.separation_margin) + 1, false);
 
+        // board number indicators
         printNumbers(renderMng);
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                board[i][j].render(renderMng, i * board_cell_size + (i + 1) * separation_margin + posX + maxNumbers * fontSize,
-                        j * board_cell_size + (j + 1) * separation_margin + posY + maxNumbers * fontSize, board_cell_size);
-            }
-        }
+        // board cells
+        for (int i = 0; i < this.width; i++)
+            for (int j = 0; j < this.height; j++)
+                this.board[i][j].render(renderMng, i * this.board_cell_size + (i + 1) * this.separation_margin +
+                                this.posX + this.maxNumbers * this.fontSize,
+                        j * this.board_cell_size + (j + 1) * this.separation_margin +
+                                this.posY + this.maxNumbers * this.fontSize, this.board_cell_size);
 
-        renderMng.setFont(fontWrongText);
-
-        if (lastTimeChecked != -1) {
+        // board fail text
+        if (this.lastTimeChecked != -1) {
             renderMng.setColor(0xFFFF0000);
-            int x = renderMng.getTextWidth(fontWrongText, "Te faltan " + checkedCells.size() + " casillas");
-            int x2 = renderMng.getTextWidth(fontWrongText, "Tienes mal " + checkedCells.size() + " casillas");
-            int y = renderMng.getTextHeight(fontWrongText);
-            renderMng.drawText((renderMng.getWidth() - x) / 2, posY - renderMng.getHeight() / 10, "Te faltan " + cellsLeft + " casillas");
-            renderMng.drawText((renderMng.getWidth() - x2) / 2, posY - renderMng.getHeight() / 10 + y * 2, "Tienes mal " + checkedCells.size() + " casillas");
+            renderMng.setFont(this.fontWrongText);
+            int textWidth = renderMng.getTextWidth(this.fontWrongText, "Te faltan " + this.checkedCells.size() + " casillas");
+            int textWidth2 = renderMng.getTextWidth(this.fontWrongText, "Tienes mal " + this.checkedCells.size() + " casillas");
+            int textHeight = renderMng.getTextHeight(this.fontWrongText);
+            renderMng.drawText((renderMng.getWidth() - textWidth) / 2, posY - renderMng.getHeight() / 10,
+                    "Te faltan " + cellsLeft + " casillas");
+            renderMng.drawText((renderMng.getWidth() - textWidth2) / 2, posY - renderMng.getHeight() / 10 + textHeight * 2,
+                    "Tienes mal " + checkedCells.size() + " casillas");
         }
     }
 
-    public void renderWin(IRender renderMng){
-        posX = (GameManager.getInstance().getWidth() - board_cell_size*width - separation_margin*(width+1))/2;
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if(board[i][j].isAnswer()){
-                    board[i][j].render(renderMng, i * board_cell_size + (i + 1) * separation_margin + posX,
-                        j * board_cell_size + (j + 1) * separation_margin + posY - GameManager.getInstance().getHeight()/10, board_cell_size);
-                }
-            }
-        }
+    public void renderWin(IRender renderMng) {
+        this.posX = (GameManager.getInstance().getWidth() - this.board_cell_size * width - this.separation_margin * (this.width + 1)) / 2;
+        for (int i = 0; i < this.width; i++)
+            for (int j = 0; j < this.height; j++)
+                if (this.board[i][j].isAnswer())
+                    this.board[i][j].render(renderMng, i * this.board_cell_size + (i + 1) * this.separation_margin + this.posX,
+                            j * this.board_cell_size + (j + 1) * this.separation_margin + this.posY - GameManager.getInstance().getHeight() / 10,
+                            this.board_cell_size);
     }
 
     public void update(double deltaTime) {
-        if (lastTimeChecked != -1) {
-            if (lastTimeChecked - deltaTime < 0) {
-                lastTimeChecked = -1;
-                for (int i = 0; i < checkedCells.size(); i++) {
-                    checkedCells.get(i).uncheck();
-                }
-                checkedCells.clear();
+        // check timer
+        if (this.lastTimeChecked != -1) {
+            if (this.lastTimeChecked - deltaTime < 0) {
+                this.lastTimeChecked = -1;
+                for (int i = 0; i < this.checkedCells.size(); i++)
+                    this.checkedCells.get(i).uncheck();
+                this.checkedCells.clear();
             } else
-                lastTimeChecked -= deltaTime;
+                this.lastTimeChecked -= deltaTime;
         }
     }
 
     private void printNumbers(IRender renderMng) {
-        renderMng.setFont(renderMng.loadFont("fonts/SimplySquare.ttf", FontType.DEFAULT, fontSize));
-        for (int i = 0; i < cols.length; i++) {
-            if (cols[i].size() == 1) {
-                renderMng.drawText(posX + board_cell_size * (i + 1) + separation_margin * i - board_cell_size / 2 + maxNumbers * fontSize,
-                        posY + maxNumbers * fontSize - 2 * separation_margin, "0");
-            }
-            for (int j = cols[i].size() - 2; j >= 0; j--) {
-                int w = cols[i].get(j);
-                renderMng.drawText(posX + board_cell_size * (i + 1) + separation_margin * i - board_cell_size / 2 + maxNumbers * fontSize,
-                        posY + maxNumbers * fontSize  - 2 * separation_margin - (fontSize * (cols[i].size() - 2 - j)), Integer.toString(w));
+        renderMng.setFont(renderMng.loadFont("fonts/SimplySquare.ttf", FontType.DEFAULT, this.fontSize));
+        for (int i = 0; i < this.cols.length; i++) {
+            if (this.cols[i].size() == 1)
+                renderMng.drawText(this.posX + this.board_cell_size * (i + 1) + this.separation_margin * i -
+                                this.board_cell_size / 2 + this.maxNumbers * fontSize,
+                        this.posY + this.maxNumbers * this.fontSize - 2 * this.separation_margin, "0");
+            for (int j = this.cols[i].size() - 2; j >= 0; j--) {
+                int w = this.cols[i].get(j);
+                renderMng.drawText(this.posX + this.board_cell_size * (i + 1) + this.separation_margin * i -
+                                this.board_cell_size / 2 + this.maxNumbers * this.fontSize,
+                        this.posY + this.maxNumbers * this.fontSize - 2 * this.separation_margin -
+                                (this.fontSize * (this.cols[i].size() - 2 - j)),
+                        Integer.toString(w));
             }
         }
 
-        for (int i = 0; i < rows.length; i++) {
-            if (rows[i].size() == 1) {
-                renderMng.drawText(posX + maxNumbers * fontSize - 8 * separation_margin,
-                        posY + board_cell_size * (i + 1) + separation_margin * i - (int) (board_cell_size / 2.5) + maxNumbers * fontSize, "0");
-            }
+        for (int i = 0; i < this.rows.length; i++) {
+            if (this.rows[i].size() == 1)
+                renderMng.drawText(this.posX + this.maxNumbers * this.fontSize - 8 * this.separation_margin,
+                        this.posY + this.board_cell_size * (i + 1) + this.separation_margin * i -
+                                (int) (this.board_cell_size / 2.5) + this.maxNumbers * this.fontSize, "0");
             for (int j = rows[i].size() - 2; j >= 0; j--) {
                 int w = rows[i].get(j);
-                renderMng.drawText(posX + maxNumbers * fontSize - 8 * separation_margin - (fontSize * (rows[i].size() - 2 - j)),
-                        posY + board_cell_size * (i + 1) + separation_margin * i - (int) (board_cell_size / 2.5) + maxNumbers * fontSize, Integer.toString(w));
+                renderMng.drawText(this.posX + this.maxNumbers * this.fontSize - 8 * this.separation_margin -
+                                (this.fontSize * (this.rows[i].size() - 2 - j)),
+                        this.posY + this.board_cell_size * (i + 1) + this.separation_margin * i -
+                                (int) (this.board_cell_size / 2.5) + this.maxNumbers * this.fontSize, Integer.toString(w));
             }
         }
     }
 
     public void check() {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if (!board[i][j].isAnswer() && board[i][j].getState() == Cell.State.MARKED) {
-                    board[i][j].setChecked();
-                    checkedCells.add(board[i][j]);
+        for (int i = 0; i < this.width; i++) {
+            for (int j = 0; j < this.height; j++) {
+                if (!this.board[i][j].isAnswer() && this.board[i][j].getState() == Cell.State.MARKED) {
+                    this.board[i][j].setChecked();
+                    this.checkedCells.add(this.board[i][j]);
                 }
             }
         }
-        lastTimeChecked = SECONDS_CHECKED;
-        if(checkedCells.size() == 0 && cellsLeft == 0)
-            win = true;
+
+        this.lastTimeChecked = SECONDS_CHECKED;
+        if (this.checkedCells.size() == 0 && this.cellsLeft == 0)
+            this.win = true;
     }
 
     public boolean isInBoard(int posX, int posY) {
-        return posX > (separation_margin + this.posX + maxNumbers * fontSize) && posX < (width * board_cell_size + width * separation_margin + this.posX + maxNumbers * fontSize)
-                && posY > (separation_margin + this.posY + maxNumbers * fontSize) && posY < (height * board_cell_size + height * separation_margin + this.posY + maxNumbers * fontSize);
+        return posX > (this.separation_margin + this.posX + this.maxNumbers * this.fontSize) &&
+                posX < (this.width * this.board_cell_size + this.width * this.separation_margin + this.posX + this.maxNumbers * this.fontSize) &&
+                posY > (this.separation_margin + this.posY + this.maxNumbers * this.fontSize) &&
+                posY < (this.height * this.board_cell_size + this.height * this.separation_margin + this.posY + this.maxNumbers * this.fontSize);
     }
 
     public void markCell(int x, int y) {
-        int boardX = (x - posX - separation_margin - maxNumbers * fontSize) - (x - posX - separation_margin - maxNumbers * fontSize) / board_cell_size * separation_margin;
-        int boardY = (y - posY - separation_margin - maxNumbers * fontSize) - (y - posY - separation_margin - maxNumbers * fontSize) / board_cell_size * separation_margin;
-        cellsLeft -= board[boardX / board_cell_size][boardY / board_cell_size].changeCell();
+        int boardX = (x - this.posX - this.separation_margin - this.maxNumbers * this.fontSize) -
+                (x - this.posX - this.separation_margin - this.maxNumbers * this.fontSize) / this.board_cell_size * this.separation_margin;
+        int boardY = (y - this.posY - this.separation_margin - this.maxNumbers * this.fontSize) -
+                (y - this.posY - this.separation_margin - this.maxNumbers * this.fontSize) / this.board_cell_size * this.separation_margin;
+        this.cellsLeft -= this.board[boardX / this.board_cell_size][boardY / this.board_cell_size].changeCell();
     }
 
     public int getCellSize() {
-        return board_cell_size;
+        return this.board_cell_size;
     }
 
     public int getMarginCells() {
-        return separation_margin;
+        return this.separation_margin;
     }
 
     public int getWidth() {
-        return cols.length;
+        return this.cols.length;
     }
 
     public int getHeight() {
-        return rows.length;
+        return this.rows.length;
     }
 }
--- Nonogram/nonogram/src/main/java/com/example/nonogram/SelectionMenu.java
+++ Nonogram/nonogram/src/main/java/com/example/nonogram/SelectionMenu.java
@@ -4,35 +4,34 @@ import com.example.engine_common.interfaces.IEngine;
 import com.example.engine_common.interfaces.IInput;
 import com.example.engine_common.interfaces.IRender;
 import com.example.engine_common.interfaces.IScene;
-import com.example.engine_common.shared.FontType;
 import com.example.engine_common.shared.InputType;
 
 public class SelectionMenu implements IScene {
     private Button t4x4;
     private Button t5x5;
-    private Button t5x10;
+    private Button t10x5;
     private Button t8x8;
     private Button t10x10;
-    private Button t10x15;
+    private Button t15x10;
     private Button backButton;
 
     @Override
     public void init() {
         int gameWidth = GameManager.getInstance().getWidth() / 3;
         int gameHeight = GameManager.getInstance().getHeight() / 6;
-        t4x4 = new Button(gameWidth / 4, gameHeight * 2, gameWidth / 2, gameWidth / 2, "4x4", "",
+        this.t4x4 = new Button(gameWidth / 4, gameHeight * 2, gameWidth / 2, gameWidth / 2, "4x4", "",
                 Resources.FONT_SIMPLY_SQUARE_MEDIUM, Resources.SOUND_BUTTON);
-        t5x5 = new Button(gameWidth * 5 / 4, gameHeight * 2, gameWidth / 2, gameWidth / 2, "5x5", "",
+        this.t5x5 = new Button(gameWidth * 5 / 4, gameHeight * 2, gameWidth / 2, gameWidth / 2, "5x5", "",
                 Resources.FONT_SIMPLY_SQUARE_MEDIUM, Resources.SOUND_BUTTON);
-        t5x10 = new Button(gameWidth * 9 / 4, gameHeight * 2, gameWidth / 2, gameWidth / 2, "5x10", "",
+        this.t10x5 = new Button(gameWidth * 9 / 4, gameHeight * 2, gameWidth / 2, gameWidth / 2, "10x5", "",
                 Resources.FONT_SIMPLY_SQUARE_MEDIUM, Resources.SOUND_BUTTON);
-        t8x8 = new Button(gameWidth / 4, gameHeight * 3, gameWidth / 2, gameWidth / 2, "8x8", "",
+        this.t8x8 = new Button(gameWidth / 4, gameHeight * 3, gameWidth / 2, gameWidth / 2, "8x8", "",
                 Resources.FONT_SIMPLY_SQUARE_MEDIUM, Resources.SOUND_BUTTON);
-        t10x10 = new Button(gameWidth * 5 / 4, gameHeight * 3, gameWidth / 2, gameWidth / 2, "10x10", "",
+        this.t10x10 = new Button(gameWidth * 5 / 4, gameHeight * 3, gameWidth / 2, gameWidth / 2, "10x10", "",
                 Resources.FONT_SIMPLY_SQUARE_MEDIUM, Resources.SOUND_BUTTON);
-        t10x15 = new Button(gameWidth * 9 / 4, gameHeight * 3, gameWidth / 2, gameWidth / 2, "10x15", "",
+        this.t15x10 = new Button(gameWidth * 9 / 4, gameHeight * 3, gameWidth / 2, gameWidth / 2, "15x10", "",
                 Resources.FONT_SIMPLY_SQUARE_MEDIUM, Resources.SOUND_BUTTON);
-        backButton = new Button(gameWidth, gameHeight * 5, gameWidth, gameHeight / 3, "Back", "",
+        this.backButton = new Button(gameWidth, gameHeight * 5, gameWidth, gameHeight / 3, "Back", "",
                 Resources.FONT_SIMPLY_SQUARE_MEDIUM, Resources.SOUND_BUTTON);
     }
 
@@ -43,13 +42,13 @@ public class SelectionMenu implements IScene {
     @Override
     public void render(IRender renderMng) {
         renderMng.setColor(0xFF000000);
-        t4x4.render(renderMng);
-        t5x5.render(renderMng);
-        t5x10.render(renderMng);
-        t8x8.render(renderMng);
-        t10x10.render(renderMng);
-        t10x15.render(renderMng);
-        backButton.render(renderMng);
+        this.t4x4.render(renderMng);
+        this.t5x5.render(renderMng);
+        this.t10x5.render(renderMng);
+        this.t8x8.render(renderMng);
+        this.t10x10.render(renderMng);
+        this.t15x10.render(renderMng);
+        this.backButton.render(renderMng);
     }
 
     @Override
@@ -62,8 +61,8 @@ public class SelectionMenu implements IScene {
             else if (t5x5.isInButton(input.getX(), input.getY())) {
                 numRows = 5; numCols = 5;
             }
-            else if (t5x10.isInButton(input.getX(), input.getY())) {
-                numRows = 5; numCols = 10;
+            else if (t10x5.isInButton(input.getX(), input.getY())) {
+                numRows = 10; numCols = 5;
             }
             else if (t8x8.isInButton(input.getX(), input.getY())) {
                 numRows = 8; numCols = 8;
@@ -71,8 +70,8 @@ public class SelectionMenu implements IScene {
             else if (t10x10.isInButton(input.getX(), input.getY())) {
                 numRows = 10; numCols = 10;
             }
-            else if (t10x15.isInButton(input.getX(), input.getY())) {
-                numRows = 10; numCols = 15;
+            else if (t15x10.isInButton(input.getX(), input.getY())) {
+                numRows = 15; numCols = 10;
             }
             else if (backButton.isInButton(input.getX(), input.getY())) {
                 engine.getSceneManager().popScene();
@@ -81,7 +80,7 @@ public class SelectionMenu implements IScene {
 
             if (numRows != 0 && numCols != 0) {
                 t4x4.clicked(engine.getAudio());
-                engine.getSceneManager().pushScene(new BoardScene(numRows, numCols));
+                engine.getSceneManager().pushScene(new BoardScene(numCols, numRows));
             }
         }
     }
